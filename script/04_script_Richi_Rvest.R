e30615a52c5cfd6ab12477c600318fc7268e3c46
Riccardo Ruta <riccardo.ruta@studenti.unimi.it>
2020-02-12 17:11:54 +0100

    ##

--- script/04_script_Richi_Rvest.R
+++ script/04_script_Richi_Rvest.R
@@ -10,7 +10,7 @@ url <- URLencode("https://www.repubblica.it")
 browseURL(url)
 
 #Downloading the page while specifying our browser and providing our email
-repubblica_page <- getURL(url, 
+repubblica_page <- RCurl:: getURL(url, 
                           useragent = str_c(R.version$platform,
                                             R.version$version.string,
                                             sep = ", "),
@@ -21,14 +21,14 @@ repubblica_page <- getURL(url,
 writeLines(repubblica_page, 
            con = here::here("repubblica.html"))
 
-#Creating an object to grab all the links
+#extract  the links
 link_pagine <- read_html(here::here("repubblica.html")) %>% 
-                          html_nodes("a") %>% 
+                          html_nodes(".block-8-2 .entry-title a , .block-8-1 a , .rep-small , .no-media a") %>%            #controllare nodo dei link
                           html_attr("href")
 
 
 #Creating a folder where to store all the pages:
-dir.create("prova_Rvest_Loop")
+dir.create("Repubblica")
 
 #Specifying lenght and mode of the vector
 articoli_repubblica <- vector(mode = "list", length = length(link_pagine))
@@ -39,20 +39,20 @@ for (i in 1:length(link_pagine)) {
   cat("Iteration:", i, ". Scraping:", link_pagine[i],"\n")
   
   #Getting the page
-  page3 <- RCurl::getURL(link_pagine[i], 
+  page <- RCurl::getURL(link_pagine[i], 
                          useragent = str_c(R.version$platform,
                                            R.version$version.string,
                                            sep = ", "),
                          httpheader = c(From = "riccardo.ruta@studenti.unimi.it"))
   
   #Saving the page:
-  file_path_1 <- here::here("prova_Rvest_Loop", str_c("pag_", i, ".html"))
-  writeLines(page3, 
-             con = file_path_1)
+  file_path <- here::here("Repubblica", str_c("pag_", i, ".html"))
+  writeLines(page, 
+             con = file_path)
   
   #Parsing and extracting
-  articoli_repubblica[[i]] <- read_html(file_path_1) %>% 
-    html_nodes(".body-text span") %>% 
+  articoli_repubblica[[i]] <- read_html(file_path) %>% 
+    html_nodes(".body-text span") %>%                  #controllare nodo del testo
     html_text()
   
   #Setting the amount of time in which the code rests.
