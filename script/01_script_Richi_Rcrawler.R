30ee360d4d7208ccd2905dbaa80c0e993477cfdd
Riccardo Ruta <riccardo.ruta@studenti.unimi.it>
2020-02-13 20:20:57 +0100

    PROGRESS WITH RCRAWLER

--- script/01_script_Richi_Rcrawler.R
+++ script/01_script_Richi_Rcrawler.R
@@ -3,26 +3,36 @@ source(here::here("script","00_setup.R"))
 
 here::here("")
 
+#test CSS selector
+test_CSS <- ContentScraper(
+  Url = "https://www.repubblica.it",
+  CssPatterns =c(".Articolo , .entry-subtitle a , .entry-title a"))
+#collect all link with xpath
+link_xpath <- ContentScraper(Url = "https://www.repubblica.it", 
+                      XpathPatterns = "//*/a/@href" ,
+                      ManyPerPattern = TRUE)
+
+
 #uso Rcrawler per scaricare tutti gli HTML della homepage di Repubblica.it
-Rcrawler (Website = "https://www.repubblica.it",
-          Useragent= "Mozilla 3.11",
-          RequestsDelay = 1,
-          MaxDepth = 1,
-          dataUrlfilter = "^https://www.repubblica.it",
-          ManyPerPattern= TRUE,
-          DIR = "./data"
+Rcrawler (Website = "https://www.repubblica.it", #LINK DEL SITO DI PARTENZA
+          Useragent= "Mozilla 3.11", #USER AGENT
+          RequestsDelay = 1, #RITARDO TRA OGNI ITERAZIONE
+          MaxDepth = 1, #LIVELLO DELLO SCRAPING
+          dataUrlfilter = "^https://www.repubblica.it", #ESCLUDI LINK DEGLI ARTICOLI A PAGAMENTO
+          ManyPerPattern = TRUE, #NON FERMARTI AL PRIMO MATCHING DEL PATTERN
+          DIR = "./data", #DOVE SALVARE IL FILE CON GLI HTML
+          ExtractCSSPat = ".Articolo , .entry-subtitle a , .entry-title a", #SEGUI SOLO LINK CON QUESTO CSS PATTERN
+          PatternsNames = "articolo" #A COSA SI REFERISCE IL PATTERN CSS
           )
 
 ListProjects()
 
-list_DATA<-LoadHTMLFiles("data/repubblica.it-131816", type = "list")
+list_DATA<-LoadHTMLFiles("data/repubblica.it-131816", type = "vector")
 
-article <- read_html("repubblica.it-111625") %>%
+article <- read_html(list_DATA) %>%
   html_nodes(".body-text > span")%>% 
   html_text()
 
-
-
-tabella <- tibble(
-  prova = vector_DATA
-)
+tabella <- tibble (
+  prova = article,
+  link = )
--- script/02_script_Richi_Rcrawler.R
+++ script/02_script_Richi_Rcrawler.R
@@ -6,12 +6,12 @@ here::here("")
 
 #estraggo tutti i link contenuti sulla homepage di repubblica
 #ottengo una lista di 3 elementi: informazioni sull'operazione, link interni, link esterni.
-repubblica_link<-LinkExtractor(url="https://www.repubblica.it/")
+repubblica_link<-LinkExtractor(url="https://www.repubblica.it/",
+                               Useragent = "Mozilla 3.11")
 
 #ispeziono l'elenco dei link interni
 repubblica_link[["InternalLinks"]]
 url_list <- c(repubblica_link[["InternalLinks"]])
-
 url_list #controllo i link
 
 #normalizzo l'elenco dei link
@@ -20,12 +20,14 @@ url_list_normalized
 
 #pulisco l'elenco tenendo solo i link che iniziano con "https://www.repubblica.it"
 url_list_cleaned <- str_subset(url_list_normalized,"^https://www.repubblica.it")
+url_list_cleaned
 
 #creo una tabella con tutti i link
 tabella_link_repubblica <- tibble(
-  elenco_link = url_list_cleaned
-)
+  elenco_link = url_list_cleaned)
 
 #extract text
-DATA<-ContentScraper(Url =url_list_cleaned, CssPatterns = c(".body-text > span , strong"),
-                     PatternsName = c("text"), ManyPerPattern = TRUE)
+DATA<-ContentScraper(Url = url_list_cleaned, CssPatterns = ".body-text > span , strong",
+                     PatternsName = "text", ManyPerPattern = TRUE)
+
+DATA
