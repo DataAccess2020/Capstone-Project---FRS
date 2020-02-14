8806adf5f062cc0d1ec4ff1339dfc1720b9b8e76
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-02-14 13:06:13 +0100

    try..

--- scr/analysis_libero.R
+++ scr/analysis_libero.R
@@ -3,6 +3,10 @@ library(tidytext)
 text_df %>%
   unnest_tokens(word, text)
 
-libero_cleaned <- dat_sort %>%
-  select(articles) %>% 
-  unnest_tokens(word, articles)
+libero_cleaned <- dat_3 %>%
+  select(sapply.dat_sort.articles..toString..windth...57.) %>% 
+  unnest_tokens(word, sapply.dat_sort.articles..toString..windth...57.)
+
+dat_3
+
+text_df <- mutate(dat_2, text = articles$articles)
