3b121dafa39adab77412e7f2fc65aedc8a47eb4a
FABIANA GEMMA GIANNUZZI <giannuzzifabianagemma@gmail.com>
2020-02-17 16:28:30 +0100

    #grouping by article #library(stopwords) #understanding how to delete stopwords

--- src/analysis.R
+++ src/analysis.R
@@ -7,6 +7,7 @@ library("tidytext")
 library("dplyr")
 library("tidyr")
 library("ggplot2")
+library("stopwords")
 
 
 # 1. vectorization 
@@ -23,7 +24,56 @@ word <- vector (mode = "character")
 dat1 <- dat %>%
   unnest_tokens (word, text)
 
-#adding line 
+#2. grouping by articles 
+dat2 <- dat1 %>%
+  group_by(link) %>%
+  mutate(linenumber = row_number())
+
+#3. 
+
+Todeletewords <- stopwords(language = "it", source = "snowball" )
+
+as.tbl(Todeletewords)
+
+stopwords <- Todeletewords
+
+
+as.tbl(stopwords, stringsAsFactor = FALSE)
+
+dat3 <- dat2 %>%
+  anti_join("stopwords" = "Todeletewords")
+
+
+
+
+y <- if (stopwords =1) {
+   0
+} else {
+  1 
+}
+
+y
+
+
+dat4 <- select(dat2, word, stopwords)
+
+dat4 <- filter(
+  dat4, 
+  word != stopwords
+)
+
 
 dat2 <- dat1 %>%
-  group_by(link)
+  count(word, sort = TRUE)
+
+
+dat2 %>%
+  count(word, sort = TRUE) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+
+
