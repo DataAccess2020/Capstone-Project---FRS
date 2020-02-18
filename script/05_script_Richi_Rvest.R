101bbc4b536fba1614e4c3f57caf1000c0493dcd
Riccardo Ruta <riccardo.ruta@studenti.unimi.it>
2020-02-18 18:10:57 +0100

    script rvest

--- script/05_script_Richi_Rvest.R
+++ script/05_script_Richi_Rvest.R
@@ -14,33 +14,34 @@ source(here::here("script","00_setup.R"))
 browseURL("https://www.repubblica.it/robots.txt")
 
 # Downloading and saving the HTML: ----------------------------------------------------
+
+#assign the url an object called "url"
 url <- URLencode("https://www.repubblica.it/")
 
+#download the page
 page <- RCurl::getURL(url, 
                       useragent = str_c(R.version$platform,
                                         R.version$version.string,
                                         sep = ", "),
                       httpheader = c(From = "riccardo.ruta@studenti.unimi.it")) 
-
+#save the HTML
 writeLines(page,
            con = here :: here("data/rvest/repubblica.html"))
 
-# Extracting the links in the homepage using Rvest:--------------------------------------
+# Extracting the links by the homepage using Rvest:--------------------------------------
 
 link <- read_html(here :: here("data/rvest/repubblica.html"))%>%
   html_nodes(css = ".Articolo , .entry-subtitle a , .entry-title a") %>%
   html_attr("href")
 
+#clean the list, remove all useless links
 link <- str_subset(link, "^https://www\\.repubblica\\.it")
 
-link
 
-#DT with links-------------
+#DT (dataset) with links-------------
 
 dat_0 <- tibble(
   link = link)
-
-dat_0
 
 #DT with links and section----------------
 
@@ -52,10 +53,15 @@ dat_1 <- tibble(
   section = section
 )
 
-# Loop for extracting the text of the files: ------------------------------------------------
+# Loop for extracting the text of all the articles ------------------------------------------------
+
+#create a folder to store the data
 dir.create("data/rvest/articles_repubblica")
+
+#create an object for the list of links
 articles <- vector(mode = "list", length = length(link))
 
+#the loop
 for (i in 1:length(link)) {
   
   cat("Iteration:", i, ". Scraping:", link[i],"\n")
@@ -81,18 +87,17 @@ for (i in 1:length(link)) {
 dat_2 <- tibble(
   link = link,
   articles = articles,
-  section = section
-)
+  section = section)
 
 
 # Sorting the dataset, deleting empty rows: -----------------
 dat_3 <- dat_2 %>%
   filter(articles != "character(0)")
 
-# clean the article row--------------
+# clean the article column--------------
 dat_4 <- data.frame(sapply(dat_3$articles, toString, windth=57))
 
-# combino dat4 e dat5---------------
+# combine dat4 e dat5---------------
 dat_5 <- cbind(dat_3, dat_4)
 
 #DT definitivo------------
@@ -100,4 +105,6 @@ dat_5 <- cbind(dat_3, dat_4)
 dat_definitivo <- tibble(link= dat_5$link,
                          section= dat_5$section,
                          text= dat_5$sapply.dat_3.articles..toString..windth...57.)
+
+#where to save definitive dataset
 save(dat_definitivo, file = here::here ("data/rvest/articoli_repubblica_17_02_2020.Rdata"))
