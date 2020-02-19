c347720844f789d9db56bff8017caa4d7ea4bad3
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-02-19 10:24:38 +0100

    plot

--- scr/analysis_libero.R
+++ scr/analysis_libero.R
@@ -1,6 +1,7 @@
 #Analysis: 
 
-library(tidytext) library(tidyr)
+library(tidytext) 
+library(tidyr)
 library(stopwords)
 library(tidyverse)
 library(wordcloud)
@@ -8,25 +9,25 @@ library(wordcloud)
 # trasforming the text from factor to character: 
 text_cleaned <- sapply(dat_3$text, toString, windth=57)
 
+# adding the new text format to the data frame: 
 dat_3 <- mutate(dat_3, text = text_cleaned)
 
 as.tbl(dat_3, stringsAsFactor = FALSE)
 
-# unnesting the token words
+# unnesting the token words into a new dataset:-----------------------------
 
 libero <- dat_3 %>%
   unnest_tokens (word, text)
 
-#adding the line numbers:
+# adding the line numbers:----------------------------------------
 
 libero <- libero %>% 
   group_by(link) %>% 
   mutate(linenumber = row_number())
 
-  
-# removing the stopwords: 
+# removing the stopwords: --------------------------------------
 
-new_stops <- c("eâ", "lâ", "â", "â")
+new_stops <- c("eâ", "lâ", "â", "â", "â")
 
 libero <- libero %>% 
   anti_join(get_stopwords(language = "it", source= "stopwords-iso")) %>%
@@ -35,22 +36,98 @@ libero <- libero %>%
   filter(!str_detect(word, '[[:punct:]]')) %>% 
   filter(!str_detect(word, new_stops)) 
 
-# saving: 
-write.csv(dat_4, file = here::here("libero.csv"))
+# saving this clean data: 
+write.csv(libero, file = here::here("libero.csv"))
 
+# I created a new data containing only the word variable, in order to calculate the frequency, without considering the links: 
 libero_words <- tibble (word = libero$word)
 
+## FREQUECIES: ------------------------------------------
+# The most frequent words in my data are: 
 libero_words %>%
-  count(word, sort = TRUE) %>% 
-  with(wordcloud(word, n, max.words = 100))
-
-libero %>%
+  count(word, sort = TRUE) 
+# Wordcloud: 
+libero_words %>%
   count(word) %>%
   with(wordcloud(word, n, max.words = 200))
+# plot for the frequecies: 
+libero_words %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 10) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
 
-# graphs
+# 1. Frequencies for the section "politica": ---------------------------
+libero %>%
+  filter(section  == "politica") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) 
+# 1.1 Wordcloud: 
+libero %>%
+  filter(section  == "politica") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) %>%
+  with(wordcloud(word, n, max.words = 100))
 
-# wordclouds: 
+# 2. Frequencies for the section "italia": -------------------------------
+libero %>%
+  filter(section  == "italia") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) 
+# 2.1 Wordcloud:
+libero %>%
+  filter(section  == "italia") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) %>%
+  with(wordcloud(word, n, max.words = 100))
+
+# 3. Frequencies for the section "personaggi": --------------------------------------------
+libero %>%
+  filter(section  == "personaggi") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) 
+# 3.1 Wordcloud:
+libero %>%
+  filter(section  == "personaggi") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) %>%
+  with(wordcloud(word, n, max.words = 100))
+
+# 4. Frequencies for the section "esteri": -------------------------------------------
+libero %>%
+  filter(section  == "esteri") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) 
+# 4.1 Wordcloud:
+libero %>%
+  filter(section  == "esteri") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) %>%
+  with(wordcloud(word, n, max.words = 100))
+
+# 5. Frequencies for the section "spettacoli": --------------------------------------
+libero %>%
+  filter(section  == "spettacoli") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) 
+# 5.1 Wordcloud:
+libero %>%
+  filter(section  == "spettacoli") %>%
+  ungroup() %>%
+  count(word, sort = TRUE) %>%
+  with(wordcloud(word, n, max.words = 100))
+
+#____________________________________________________________
+
+
+
+
+
+
+
 
 
 
