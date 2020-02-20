3b966581fe6de359467ac6ffe8d698204a23db90
Sofia Ragazzi <sofiagiovanna.ragazzi@studenti.unimi.it>
2020-02-20 18:40:20 +0100

    tokens, stemming and n-words

--- scr/try_sentiment.R
+++ scr/try_sentiment.R
@@ -23,7 +23,7 @@ parole_libero <- str_c(parole_libero, collapse = " ")
 
 # CORPUS:----------------------------------------------------- 
 corpus <- corpus(
-  parole_libero
+  dat_character$text
 )
 summary(corpus)
 names(corpus)
@@ -35,10 +35,68 @@ kwic(corpus, "salvini")
 
 
 ## TOKENS: ------------------------------------------------------
-corp_it_frasi <- corp_it %>%
+corpus_frasi <- corpus %>%
   corpus_reshape(to = "sentences")
 # I testi ora sono frasi singole
-texts(corp_it_frasi)[3]
+texts(corpus_frasi)[3]
+
+# rimuovo numeri e punteggiatura: 
+token_frasi <- corpus_frasi %>%
+  tokens(remove_punct = T,
+         remove_numbers = T,
+         verbose = T)
+
+head(token_frasi[[6]])
+
+# rimuovo apostrofi: 
+texts(corpus_frasi) <- texts(corpus_frasi) %>%
+  str_replace_all("([a-z])\\'", "\\1 ")
+
+token_frasi <- corpus_frasi %>%
+  tokens(remove_punct = T,
+         remove_numbers = T)
+head(token_frasi[[6]], n = 7)
+
+#STEMMING:---------------------------------------------
+
+#stopwords: 
+stp <- c(stopwords("italian"), "d", "eâ", "lâ", "â", "â", "â")
+
+# rimuovo sia stopwords che maiuscole: 
+token_frasi <- token_frasi %>%
+  tokens_tolower() %>%                     # Porta tutto in minuscolo
+  tokens_remove(stp) %>%                   # Rimuove stop words (lista aggiornata da noi)
+  tokens_wordstem(language = "italian")    # Stemming
+head(token_frasi[[1]], n = 10)
+
+# N - GRAMMI: ---------------------------------------------------
+
+ngr <- textstat_collocations(
+  token_frasi, 
+  size = 2:3,                 # Sequence di 2 e 3 parole
+  min_count = 3) 
+
+ngr %>%
+  arrange(-count)
+
+# unisco all'oggetto precedente: 
+
+token_frasi <- tokens_compound(
+  token_frasi, 
+  phrase(ngr$collocation), 
+  join = T
+)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
