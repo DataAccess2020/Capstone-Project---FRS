3bd5f2686af8f01035ff90f1fc40121f7dc33d8b
Riccardo Ruta <riccardo.ruta@studenti.unimi.it>
2020-02-22 18:16:44 +0100

    quanteda

--- script/07_Analysis_quanteda.R
+++ script/07_Analysis_quanteda.R
@@ -7,21 +7,21 @@ load(here::here("data/rvest/articoli_repubblica_17_02_2020.Rdata"))
 #remove double links-------------
 dataset_pulito <- unique (dat_definitivo)
 
-#pulisco il testo from factor to character---------------
+#clean the text from factor to character---------------
 text_cleaned <- sapply(dataset_pulito$text, toString, windth=57)
 
-#sostituisco nel dataframe definitivo la colonna text con character (al posto del factor)--------
+#replace in the dataset--------
 dataset_pulito <- mutate(dataset_pulito, text = text_cleaned)
 
-#creo tabella con dataset definitvo-----------------
+#table with definitive dataset-----------------
 as.tbl(dataset_pulito, stringsAsFactor = FALSE)
 
-#creo corpus per usare quanted
+#creating a corpus in order to use quanteda
 corpus <- corpus(dataset_pulito)
 
 summary(corpus)
 
-#divido il teso in parole
+#divide text in words
 tokens(corpus, "word")
 
 #divido il testo degli articoli in frasi
