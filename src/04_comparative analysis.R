8845ff71092209f51293fa70e8bfaff7772990d1
Your Name <giannuzzifabianagemma@gmail.com>
2020-02-24 15:50:11 +0100

    TRYING TO ADD SENTIMENT IN COMPARATIVE SCRIPT

--- src/04_comparative analysis.R
+++ src/04_comparative analysis.R
@@ -43,6 +43,8 @@ jac_sim
 
 SECTIONS <- filter(NEWSPAPERS, section == "esteri" | section =="cronache" | section=="politica"| section=="economia" | section== "la-lettura" | section== "scuola") 
 
+table(NEWSPAPERS$section)
+
 #Creating corpus 
 corp1 <- corpus(
   SECTIONS
@@ -68,4 +70,79 @@ jac_sim1 <- sections_dtm %>%
                  margin = "documents") 
 jac_sim1
 
+#sentiment graph 
+
+# CREATING OPENER
+opeNER_xml <- read_xml("./dictionary/it-sentiment_lexicon.lmf.xml")
+entries <- xml_find_all(opeNER_xml, ".//LexicalEntry")
+lemmas <- xml_find_all(opeNER_xml, ".//Lemma")
+confidence <- xml_find_all(opeNER_xml, ".//Confidence")
+sentiment <- xml_find_all(opeNER_xml, ".//Sentiment")
+
+# Parse and put in a data frame
+opeNER_df <- data.frame(
+  id = xml_attr(entries, "id"),
+  lemma = xml_attr(lemmas, "writtenForm"),
+  partOfSpeech = xml_attr(entries, "partOfSpeech"),
+  confidenceScore = as.numeric(xml_attr(confidence, "score")),
+  method = xml_attr(confidence, "method"),
+  polarity = as.character(xml_attr(sentiment, "polarity")),
+  stringsAsFactors = F)
+
+
+# opeNER_df --------
+# Fix a mistake (join "nneutral" and "neutral")
+opeNER_df$polarity <- ifelse(opeNER_df$polarity == "nneutral", 
+                             "neutral", opeNER_df$polarity)
+
+
+# Make quanteda dictionary 
+opeNER_dict <- quanteda::dictionary(with(opeNER_df, split(lemma, polarity)))
+
+# Saving it locally
+write.csv(opeNER_df, file = here::here("dictionary","opeNER_df.csv"))
+
+# Importing dictionary 
+opeNER <- rio::import("./dictionary/opeNER_df.csv")
+head(opeNER)
+
+# deleting words without polarity
+table(opeNER$polarity, useNA = "always")     
+opeNER <- opeNER %>%                       #obs change 25098 - 25053
+  filter(polarity != "")
+
+#SENTIMENT GRAPH 
+
+# Creare dizionario dal lessico opeNER
+opeNERdict <- quanteda::dictionary(
+  split(opeNER$lemma, opeNER$polarity)
+)
+lengths(opeNERdict)
+
+library(quanteda)
+
+news_dtm <- dfm(
+  newspapers_dtm,
+  tolower = T,
+  dictionary = opeNERdict
+) %>%
+  dfm_group(
+    group = "newspaper"
+  )
+
+
+head(news_dtm)
+
+
+quanteda::convert(news_dtm,
+                  to = "data.frame") %>%
+  rename(newspaper = document) %>%
+  mutate(
+    polar = (positive - negative)/(positive + negative)
+  ) %>%
+  ggplot(., aes(y = reorder(newspaper, polar), x = polar)) +
+  geom_point() +
+  ylab("") +
+  xlab("Polarità sentiment\n(da negativo a positivo)") +
+  theme_bw()
 
